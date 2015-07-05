520c47fbaa021d0c3908f4ad8fb704dc04d83e55
Hubert Denkmair <hubert@denkmair.de>
2015-07-05 13:22:52 +0200

    added sample script for endless recording

---
+++ infinite_record.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+	tmpfile=$(tempfile --suffix=.mid)
+	./arecordmidi -p 24 -T 10000 $tmpfile
+
+	if [ -n $tmpfile ]
+	then
+		outfile=$(date +arecordmidi-%Y-%m-%d-%H:%M:%S.mid)
+		mv $tmpfile ~/midi/$outfile
+	else
+		rm $tmpfile
+		sleep 5
+	fi
+done
+
